d4a2b29f90ae26f351446c3e4be1ac6a9d08e4f0
Alexandre Campinho <alexarc@hey.com>
2021-10-17 17:57:00 +1300

    Add final portal

--- main.ts
+++ main.ts
@@ -3,19 +3,22 @@ namespace SpriteKind {
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (charmander.vy == 0) {
-        charmander.vy = -100
+        charmander.vy = -150
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.donuts, function (sprite, otherSprite) {
     info.changeScoreBy(1)
     otherSprite.destroy()
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    game.over(true)
+})
 let Donuts: Sprite = null
 let charmander: Sprite = null
 scene.setBackgroundColor(9)
 charmander = sprites.create(assets.image`Charmander`, SpriteKind.Player)
 controller.moveSprite(charmander, 100, 0)
-tiles.setTilemap(tilemap`level1`)
+tiles.setTilemap(tilemap`Level 1`)
 charmander.ay = 200
 scene.cameraFollowSprite(charmander)
 for (let value of tiles.getTilesByType(assets.tile`myTile1`)) {
